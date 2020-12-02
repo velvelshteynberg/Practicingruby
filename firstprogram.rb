ba9f50efa9e5467723e7517f72cebea6ec7da1aa
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-01 22:50:38 -0500

    THis is my first attempt of ruby

---
+++ firstprogram.rb
@@ -0,0 +1,73 @@
+puts "Hello world"
+
+puts 1+1
+
+
+puts 10*10
+
+
+puts 10.0/3
+
+
+puts 10/3.0
+
+
+puts 99/8.0
+
+
+puts 'Hello world'
+
+
+# Learning the string interpolation command
+puts "the sum of the numbers is #{10+4}"
+
+
+puts "the sum of the nubmers is #{77-78}"
+
+
+#boolean
+puts "#{2+2 == 4}"
+puts "#{3-2 == 2}"
+
+
+my_name = "velvel"
+
+
+his_name = "nil"
+
+
+puts my_name
+
+puts "hi my name is #{my_name}"
+
+
+
+puts "hi my name is #{his_name}"
+
+SERVICE_CHARGE = $5
+
+
+my_fav_num = 13
+
+
+puts my_fav_num + 13
+
+
+my_fav_num = my_fav_num + 13
+
+
+puts my_fav_num + 13
+
+
+my_fav_num = my_fav_num.to_s
+
+
+ puts my_fav_num + my_name
+
+
+
+
+
+
+
+
