09725e711290cb024147d6073d8b549cdc3abc2f
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-02 19:49:03 -0500

    practicing numbers, strings, booleans,varibales and constants and converting integers to strings

---
+++ firstpracticeruby.rb
@@ -0,0 +1,73 @@
+#Different types of data
+
+
+#numbers
+
+puts 10-10
+
+
+puts 100.0/8987
+
+
+puts 4+9
+
+
+puts 69*96
+
+
+
+#strings
+
+puts "This is the topic sentence: I am now discussing the string data type in Ruby"
+
+
+puts 'This is the topic sentence: I am now discussing the string data type in Ruby'
+
+
+puts "I am computing that 6*9 is #{6*9}"
+
+
+#Boolean data type
+
+puts 2 == 3*1
+
+puts 45 != 3*15
+
+puts 1 == 2-1
+
+puts 17 == 7+10
+
+
+puts "#{22 == 2*11}"
+
+
+#Variable and Constants
+
+my_number_one_idea = "showering is a good idea"
+
+FAVOURITE_COMMENT = "go shower"
+
+favourite_number = 18
+
+favourite_number += 7
+
+puts my_number_one_idea + FAVOURITE_COMMENT
+
+
+
+
+
+puts favourite_number
+
+favourite_number =- 11
+
+favourite_number = favourite_number.to_s
+
+
+puts my_number_one_idea + FAVOURITE_COMMENT + favourite_number
+
+puts favourite_number
+
+
+
+
