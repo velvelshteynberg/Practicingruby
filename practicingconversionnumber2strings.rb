2f4b615ce1a293f32db27d609940e5c55100d8e3
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-03 22:10:29 -0500

    first attempt

---
+++ practicingconversionnumber2strings.rb
@@ -0,0 +1,20 @@
+my_favourite_number = 99
+
+
+
+my_favourite_hobby = "playing soccer"
+
+# i dont understand why this is not working
+puts my_favourite_number + my_favourite_number
+
+
+my_favourite_number =  my_favourite_number.to_s 
+
+
+my_favourite_hobby = my_favourite_hobby.to_i
+
+
+puts  my_favourite_hobby + my_favourite_number 
+
+
+puts my_favourite_hobby +  my_favourite_hobby
